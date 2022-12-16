88a16b92465e760ddc0a27b2484dc1b15a043e9b
Parth Vakil <pmv54@cornell.edu>
2022-12-16 01:32:06 -0500

    kalign script done

--- pv_script.py
+++ pv_script.py
@@ -1,9 +1,17 @@
 import os
 
+# working_directory = os.getcwd()
+# for i in range(14, 15):
+#   print(i)
+#   input = "/muscle_input%d' > '" % i
+#   os.system(working_directory + "/muscle-main/src/Darwin/muscle" + "-align" + input + "-output" + "/muscle_output14.fasta")
+
+# print(os.getcwd())
+
 working_directory = os.getcwd()
 for i in range(14, 15):
   print(i)
-  input = "/muscle_input%d' > '" % i
-  os.system(working_directory + "/muscle-main/src/Darwin/muscle" + "-align" + input + "-output" + "/muscle_output14.fasta")
-
+  input = "/input_alignments/RV30_BB300%d.fasta' > '" % i
+  os.system("kalign -i '" + working_directory + input + working_directory + "/kalign_output.fasta'")
+print("hi %d ok" % 5)
 print(os.getcwd())
